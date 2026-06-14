360a086fc0d35d07b9b2829dbabde59cdb5f7415
solvrx <79269857+Fokkss@users.noreply.github.com>
2026-06-15 00:14:11 +0300

    cpp done! (#47)
    * 7 ticket
    
    * up to 9
    
    * up to 12
    
    * up to 14
    
    * - added gitignore
    
    * up to 19
    
    * 19 addings
    
    * begin 21
    
    * 21 ticket
    
    * delete trash
    
    * 22 ticket
    
    * 23
    
    * 25
    
    * 26
    
    * 27
    
    * 28
    
    * 32
    
    * 33
    
    * up to 40
    
    * improved gitignore
    
    * up to 47
    
    * 56
    
    * 66
    
    * up to 74
    
    * small fixes, up to 77
    
    * small changes
    
    * fixed up to 7, added tex
    
    * small fixes up to 40
    
    * fixes up to 72
    
    * finalizing algebra, cpp tickets added
    
    ticket 1 done
    
    * up to 3rd ticket
    
    * up to 6, lectures added
    
    * up to 7
    
    * cpp practice with lukyanov added
    
    * up to 9
    
    * up to 10
    
    * up to 12
    
    * up to 13
    
    * up to 14
    
    * up to 16
    
    * up to 20
    
    * all + bonuses

--- holy_c/include/ticket_lib9.hpp
+++ holy_c/include/ticket_lib9.hpp
@@ -1,2 +1,110 @@
 #pragma once
 
+#include <cstddef>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+template <typename T, size_t Rows, size_t Cols>
+class StaticMatrix
+{
+private:
+    T data_[Rows * Cols];
+
+    size_t index(size_t row, size_t col) const
+    {
+        if (row >= Rows || col >= Cols)
+        {
+            throw out_of_range("StaticMatrix index is out of range");
+        }
+
+        return row * Cols + col;
+    }
+
+public:
+    StaticMatrix()
+        : data_{}
+    {
+    }
+
+    size_t rows() const
+    {
+        return Rows;
+    }
+
+    size_t cols() const
+    {
+        return Cols;
+    }
+
+    T& operator()(size_t row, size_t col)
+    {
+        return data_[index(row, col)];
+    }
+
+    const T& operator()(size_t row, size_t col) const
+    {
+        return data_[index(row, col)];
+    }
+
+    void fill(const T& value)
+    {
+        for (size_t i = 0; i < Rows * Cols; ++i)
+        {
+            data_[i] = value;
+        }
+    }
+
+    StaticMatrix& operator+=(const StaticMatrix& other)
+    {
+        for (size_t i = 0; i < Rows * Cols; ++i)
+        {
+            data_[i] += other.data_[i];
+        }
+
+        return *this;
+    }
+
+    void print(ostream& out) const
+    {
+        for (size_t row = 0; row < Rows; ++row)
+        {
+            for (size_t col = 0; col < Cols; ++col)
+            {
+                out << (*this)(row, col) << " ";
+            }
+
+            out << endl;
+        }
+    }
+};
+
+template <typename T, size_t Rows, size_t Cols>
+StaticMatrix<T, Rows, Cols> operator+(
+    StaticMatrix<T, Rows, Cols> left,
+    const StaticMatrix<T, Rows, Cols>& right
+)
+{
+    left += right;
+    return left;
+}
+
+template <typename T, size_t Rows, size_t Cols>
+T trace(const StaticMatrix<T, Rows, Cols>& matrix)
+{
+    static_assert(Rows == Cols, "trace requires square matrix");
+
+    T result = {};
+
+    for (size_t i = 0; i < Rows; ++i)
+    {
+        result += matrix(i, i);
+    }
+
+    return result;
+}
+
+template <typename T>
+using Matrix3x3 = StaticMatrix<T, 3, 3>;
+
--- holy_c/src/ticket9.cpp
+++ holy_c/src/ticket9.cpp
@@ -1,5 +1,25 @@
 #include "ticket_lib9.hpp"
 
-int main() {
+int main()
+{
+    Matrix3x3<int> a;
+    Matrix3x3<int> b;
+
+    a.fill(1);
+    b.fill(2);
+
+    a(0, 0) = 10;
+    b(1, 1) = 20;
+
+    Matrix3x3<int> c = a + b;
+
+    c.print(cout);
+
+    cout << "trace = " << trace(c) << endl;
+
+    StaticMatrix<int, 2, 3> notSquare;
+
+    // cout << trace(notSquare) << endl; // compile-time error
+
     return 0;
 }
